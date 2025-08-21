a1ae1cb71bc783abc589525528d04deb73587c9d
Nishant Gautam <gautamniishant@icloud.com>
2025-08-21 15:07:50 -0400

    chore: Initial Development build

---
+++ calmmind-app/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colorBlack: "#000000",
+  colorWhite: "#FFFFFF",
+  colorGray: "#808080",
+  colorLightGray: "#D3D3D3",
+  colorDarkGray: "#A9A9A9",
+  colorVeryDarkGray: "#808080",
+  colorVeryLightGray: "#D3D3D3",
+};
