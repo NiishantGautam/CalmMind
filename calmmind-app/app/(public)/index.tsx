791fe357ffe1ac99da75c7912b768a747505dbd9
Nishant Gautam <gautamniishant@icloud.com>
2025-08-21 20:33:25 -0400

    feat: Add Authentication RootLayout

--- calmmind-app/app/(public)/index.tsx
+++ calmmind-app/app/(public)/index.tsx
@@ -5,7 +5,7 @@ import { theme } from "@/theme";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Auth Screen</Text>
       <StatusBar style="auto" />
     </View>
   );
--- calmmind-app/app/_layout.tsx
+++ calmmind-app/app/_layout.tsx
@@ -1,9 +1,30 @@
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 
-export default function Layout() {
+function RootLayoutWithAuth() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    // Loader Screen
+    return null;
+  }
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+      <Stack.Screen name="(public)" options={{ headerShown: false }} />
     </Stack>
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <RootLayoutWithAuth />
+    </ClerkProvider>
   );
 }
